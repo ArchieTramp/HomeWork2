74ad4bc91c641487c0f5b2c8e1481d6b61f42d9e
ArchieTramp <64407929+ArchieTramp@users.noreply.github.com>
2020-04-30 16:58:44 +0500

    final update for consult

--- src/generator.java
+++ src/generator.java
@@ -19,14 +19,14 @@ public class generator {
                 if ((myArrays[i] * myArrays[i]) == myArray[i]) {
                     System.out.println(myArray[i]);
                 }
-            } else try {
-            } catch (Exception e) {
-                throw new Exception(e);
+            } else throw new Exception("e");// try {
+//            } catch (Exception e) {
+//                throw new Exception(e);
 
-            } finally {
-                System.out.println("в массив было сгенерировано отрицательное число в элемент " + i);
+//            } finally {
+//                System.out.println("в массив было сгенерировано отрицательное число в элемент " + i);
 
             }
         }
     }
-}
+
